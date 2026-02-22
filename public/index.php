090336b786553d6b81bf895dcde4196522de8dd0
jesuu194 <jesus.lopezdonate@cieep.com>
2026-02-23 00:03:00 +0100

    Test if Render is using this code

--- public/index.php
+++ public/index.php
@@ -1,11 +1,2 @@
 <?php
-echo "PHP funciona en Render<br>";
-
-try {
-    $db = new PDO('sqlite:/tmp/test.db');
-    echo "SQLite connection successful<br>";
-    $db->exec("CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT)");
-    echo "Table created or already exists<br>";
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage() . "<br>";
-}
+echo "SI VES ESTO, RENDER USA ESTE CODIGO";
